1f9f7a5240a0b67a628f4ad9b88011d4749066f1
Lakshmi <lakshmi.nishthala@gmail.com>
2017-12-05 08:29:54 -0800

    Initial setup done

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports =
+{
+  "development": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": "okodin_development",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": "okodin_tes",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": "okodin_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
